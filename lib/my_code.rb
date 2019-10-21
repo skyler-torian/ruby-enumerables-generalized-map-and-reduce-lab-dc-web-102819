5be9561b6f4ce8bc3cd05c7bfa5f20a94038435b
Skyler Torian <skymanat7@yahoo.com>
2019-10-21 17:57:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,24 +1,24 @@
 # Your Code Here
-def map( arr )
+def map(arr)
   mapped_arr = Array.new
   index = 0
   while index < arr.size do
-    mapped_arr.push( yield( arr[index] ) )
+    mapped_arr.push(yield(arr[index]))
     index += 1
   end
   mapped_arr
 end
 
-def reduce( arr, starting_point = 0 )
+def reduce(arr, starting_point = 0)
   result = starting_point
   index = 0
 
   while index < arr.size do
-    result = yield( result, arr[index] )
+    result = yield(result, arr[index])
     index += 1
     p result
   end
-  if ( result == 0 )
+  if (result == 0)
     return true
   end
   result
